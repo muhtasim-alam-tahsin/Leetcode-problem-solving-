292298273c90d670eb2b0eaf4062cb073fd9e470
muhtasim-alam-tahsin <muhtasimtahsin258.a@gmail.com>
2025-01-30 07:00:58 +0600

    Add files via upload

---
+++ Contains_duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_set<int>s;
+        for(int i=0;i<n;i++){   
+            if(i>k) s.erase(nums[i-k-1]);
+            if(!s.insert(nums[i]).second) return true;
+        }
+        return false;
+    }
+};
