1ff83508b8e81aefc6491ff4a20ac0df0bbebfd6
muhtasim-alam-tahsin <muhtasimtahsin258.a@gmail.com>
2025-01-30 07:03:21 +0600

    Update Contains_duplicate.cpp

--- Contains_duplicate.cpp
+++ Contains_duplicate.cpp
@@ -1,3 +1,17 @@
+NOTE!!!!
+
+insert(nums[i]).second -> checks if nums[i] was successfully inserted.
+!seen.insert(nums[i]).second means "If nums[i] was already in the set", return true 
+(because we found a duplicate within the given range k).
+
+unordered_set<int> returns a pair:
+first: An iterator pointing to the element in the set.
+second: A boolean (true or false).
+true → Insertion was successful (element was not already present).
+false → Element already existed in the set (duplicate found).
+
+
+
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
